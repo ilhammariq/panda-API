146476061e854358afb50587ebd0f95cd9cd6e29
ilhammariq <ilhammuhammadariq1@gmail.com>
2021-02-23 16:01:24 +0700

    Ariq

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+    res.send(req.params)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
